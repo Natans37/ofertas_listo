19608d25bc0a6e892bead168ae02e9003c203cc8
Monitoracao Listo <monitoracaolisto@soulisto.com.br>
2023-03-31 10:00:41 -0300

    Update views.py

--- auctions/views.py
+++ auctions/views.py
@@ -375,7 +375,8 @@ def login_view(request):
             return render(request, "auctions/login.html", {
                 "message": "Invalid username and/or password."
             })
-  
+    else:
+        return render(request, "auctions/login.html")
 
 def logout_view(request):
     logout(request)
